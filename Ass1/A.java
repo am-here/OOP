44898fd3da0a6b520be4fa855c398bba642be71a
Harsh Baid <74610712+am-here@users.noreply.github.com>
2022-12-01 11:48:35 +0530

    Update A.java

--- Ass1/A.java
+++ Ass1/A.java
@@ -1,6 +1,6 @@
 class A {
     public static void main(String[] args) {
         System.out.print("MCKVIE\nComputer Science & Engineering");
-        System.out.println("HEY YOU JOE");
+        System.out.println("HEY YOU!");
     }
 }
